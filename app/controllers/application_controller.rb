afa8f9e1abba321d8c31fdd2b55fb39574168003
yamachan32 <apropiado.para.m@gmail.com>
2022-02-28 14:08:54 +0900

    ユーザー認証機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+    # deviseアクション ユーザーのサインイン状態で画面遷移振り分け
+    before_action :authenticate_user! 
+
+    # deviseカラムのパラメータ設定 deviseコントローラーへ振り分ける時に実行
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    private
+    # sanitizerメソッド deviseコントローラーはパラメーター設定編集ができない
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    end
 end
