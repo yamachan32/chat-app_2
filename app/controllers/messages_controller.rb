fa02c94b70fbff4c396976a99077eb8891bc03c6
yamachan32 <apropiado.para.m@gmail.com>
2022-02-28 21:12:44 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,10 @@
 class MessagesController < ApplicationController
 
+
   def index
     @message = Message.new
     @room = Room.find(params[:room_id])
+    @messages = @room.messages.includes(:user)
   end
 
   def create
@@ -12,6 +14,7 @@ class MessagesController < ApplicationController
       redirect_to room_messages_path(@room)
     else
       render :index
+      @messages = @room.messages.includes(:user)
     end
   end
   private
